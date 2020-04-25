a9662e4e7bd3f314ac724bafbd1031418dd4dfb6
Ronald Miller <aurora_secondary@RIM.local>
2020-04-24 23:03:37 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,25 @@
+# imports
+import os
+import json
+# import requests
+
+
+'''
+Goal is to make some sort of tool using
+Spotify API
+'''
+
+# Relearning Python OOP
+class User:
+    name = "Name"
+
+
+def get_user(self, *args):
+    return self.name[0], args
+
+# def print_user(User)
+
+user = User()
+print(user.name)
+print(get_user(user, "Test", "Test1"))
+print(os.getcwd())
